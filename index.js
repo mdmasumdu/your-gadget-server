6ae78a1fce8222fcf1928d8781c232c3fe426f17
MD MASUM <mdmasumdu45@gmail.com>
2023-10-19 02:18:58 +0600

    create another get operation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port =process.env.PORT|| 5000;
 
@@ -36,6 +36,12 @@ async function run() {
    app.get('/products',async (req,res)=>{
     const cursor = productCollection.find()
     const result = await cursor.toArray()
+    res.send(result)
+   })
+   app.get('/products/:id',async (req,res)=>{
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await productCollection.findOne(query)
     res.send(result)
    })
     app.post("/addproduct",async (req,res)=>{
