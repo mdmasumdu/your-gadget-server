a5eb88884192b1a093f9fea67a746487e30681fa
MD MASUM <mdmasumdu45@gmail.com>
2023-10-19 15:40:17 +0600

    created get,post and delete operation for cart apis

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ async function run() {
     await client.connect();
 
    const productCollection = client.db('ProductDB').collection('Products')
+   const carts = client.db('ProductDB').collection('cart')
 
 
    app.get('/products',async (req,res)=>{
@@ -38,21 +39,75 @@ async function run() {
     const result = await cursor.toArray()
     res.send(result)
    })
+
+
    app.get('/products/:id',async (req,res)=>{
     const id = req.params.id;
     const query = {_id: new ObjectId(id)}
     const result = await productCollection.findOne(query)
     res.send(result)
    })
+
+
     app.post("/addproduct",async (req,res)=>{
 
     const newProduct = req.body;
     const result = await productCollection.insertOne(newProduct)
       res.send(result)
-
-   
-   
+      
     })
+
+
+
+    app.put('/update/:id',async (req,res)=>{
+      const id =req.params.id;
+      const product = req.body;
+      const filter = { _id:new ObjectId(id) };
+      const options = { upsert: true };
+
+      const updateProduct = {
+        $set: {
+          productname:product.productname,
+          brandname:product.brandname,
+          price:product.price,
+          rating:product.rating,
+          shortdescription:product.shortdescription,
+          photo:product.photo
+        },
+      };
+
+
+      const result = await productCollection.updateOne(filter,updateProduct,options)
+      res.send(result)
+    })
+
+
+
+
+// cart apis
+    app.post("/cart",async (req,res)=>{
+      const product =req.body;
+      const result = await carts.insertOne(product)
+      res.send(result)
+      
+    })
+
+    app.get('/cart',async (rew,res)=>{
+      const cursor = carts.find()
+      const result = await cursor.toArray()
+      res.send(result)
+
+    })
+
+    app.delete('/cart/:id',async(req,res)=>{
+      const id = req.params.id
+      console.log(id)
+      const query = {_id:id };
+      const result = await carts.deleteOne(query);
+    res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
